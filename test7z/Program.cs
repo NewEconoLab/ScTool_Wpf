b64ec5eb2e56c57bee14d38421e867e8fc393f73
lights <lightsever@hotmail.com>
2017-12-25 11:54:58 +0800

    整理一个不依赖windows 的7z解压版本。

---
+++ test7z/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace test7z
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bytes = System.IO.File.ReadAllBytes("d:\\neo\\2.7z");
+            SevenZip.Pack.SevenzipFile file = new SevenZip.Pack.SevenzipFile(bytes);
+            var unpack = file.Unpack();
+            System.IO.File.WriteAllBytes("d:\\neo\\2.json", unpack);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
